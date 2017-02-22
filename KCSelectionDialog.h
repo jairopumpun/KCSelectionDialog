0af9c69a69ad90ef137ceeaef0ce11c2781d49b6
Jairo Chapela-Martínez <jairo@pumpun.com>
2017-02-22 15:50:52 +0100

    lost files

---
+++ KCSelectionDialog.h
@@ -0,0 +1,19 @@
+//
+//  KCSelectionDialog.h
+//  KCSelectionDialog
+//
+//  Created by Jairo Chapela on 21/02/17.
+//  Copyright © 2017 pumpun4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCSelectionDialog.
+FOUNDATION_EXPORT double KCSelectionDialogVersionNumber;
+
+//! Project version string for KCSelectionDialog.
+FOUNDATION_EXPORT const unsigned char KCSelectionDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCSelectionDialog/PublicHeader.h>
+
+
